6df904423e8052b25da1919b611fac984eb0c473
joseluisgs <joseluisgonsan@gmail.com>
2020-01-14 13:29:35 +0100

    Initial commit

---
+++ src/main/java/com/joseluisgs/walaspringboot/WalaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.joseluisgs.walaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalaSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalaSpringBootApplication.class, args);
+    }
+
+}
